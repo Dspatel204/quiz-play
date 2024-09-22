15d7b450e177bfe99e108926ec3d721daa1f8550
Dspatel204 <dishant.sureshbhai@gmail.com>
2024-09-22 21:05:03 +0530

    adda first project

---
+++ src/Assets/utils/landingCarouselData.js
@@ -0,0 +1,60 @@
+import kbcQuiz from '../images/QuizImages/kbcQuiz.jpg';
+import scienceQuiz from '../images/QuizImages/scienceQuiz.png';
+import photoQuiz from '../images/QuizImages/photoQuiz.jpg';
+import movieQuiz from '../images/QuizImages/movieQuiz.png';
+import bigOnlineQuiz from '../images/QuizImages/bigOnlineQui.jpg';
+import memoryGames from '../images/QuizImages/memoryGames.jpg';
+import vegQuiz from '../images/QuizImages/veg.png';
+
+const landingCarouselData = [
+  {
+    image: kbcQuiz,
+    title: "KBC Quiz",
+    btn: "NEW",
+    detail: "KBC online quiz game details.",
+  },
+  {
+    image: scienceQuiz,
+    title: "Science Quiz",
+    btn: "NEW",
+    detail: "Science online quiz game details.",
+  },
+  {
+    image: photoQuiz,
+    title: "Photo Quiz",
+    btn: "NEW",
+    detail: "Photo quiz online game details.",
+  },
+  {
+    image: movieQuiz,
+    title: "Movie Quiz",
+    btn: "NEW",
+    detail: "Movie online quiz game details.",
+  },
+  {
+    image: bigOnlineQuiz,
+    title: "Big Quiz",
+    btn: "NEW",
+    detail: "Big online quiz game details.",
+  },
+  {
+    image: memoryGames,
+    title: "Memory Quiz",
+    btn: "NEW",
+    detail: "Memory online quiz game details.",
+  },
+  {
+    image: kbcQuiz,
+    title: "Adventure Quiz",
+    btn: "NEW",
+    detail: "Adventure online quiz game details.",
+  },
+  {
+    image: vegQuiz,
+    title: "Vegetable Quiz",
+    btn: "NEW",
+    detail: "Vegetable online quiz game details.",
+  },
+];
+
+export default landingCarouselData;
